41040c30f1fe1e4fea0483162f64174393a082fc
Ingmar Splitt <orgua@users.noreply.github.com>
2025-05-09 14:30:30 +0200

    basic sphinx project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Shepherd-Nova
-Public instance of the Shepherd Testbed
+# Shepherd Nova
+
+[![QA-Tests](https://github.com/orgua/shepherd/actions/workflows/qc_tests.yml/badge.svg)](https://github.com/orgua/shepherd/actions/workflows/quality_assurance.yaml)
+[![Documentation](https://github.com/orgua/shepherd/actions/workflows/sphinx_to_pages.yml/badge.svg)](https://nes-lab.github.io/shepherd-nova/)
+[![PyPiVersion](https://img.shields.io/pypi/v/shepherd_herd.svg)](https://pypi.org/project/shepherd_data)
+
+Public Instance of the Shepherd Testbed
---
+++ docs/conf.py
@@ -0,0 +1,48 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Shepherd Nova'
+project_full = "Public Instance of the Shepherd Testbed"
+copyright = '2025, Ingmar Splitt'
+author = 'Ingmar Splitt'
+release = '0.1.0'
+builder = "html"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinxawesome_theme",
+    "sphinx_sitemap",
+    "myst_parser",
+]
+
+templates_path = ['_templates']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+language = "en"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinxawesome_theme'
+
+html_theme_options = {
+    "show_scrolltop": True,
+    "show_prev_next": True,
+#    "main_nav_links": {
+#    },
+}
+# TODO: https://sphinxawesome.xyz/how-to/options/
+
+html_baseurl = "https://nes-lab.github.io/Shepherd-Nova/"
+html_permalinks_icon = "<span>#</span>"
+html_static_path = ['_static']
+html_extra_path = ["robots.txt"]
+
+sitemap_url_scheme = "{link}"
